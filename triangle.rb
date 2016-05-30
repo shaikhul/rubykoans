239bff062d6840b66b3ad9af9f27177ca8da6fed
Shaikhul Islam Chowdhury <shaikhul.net@gmail.com>
2016-05-30 13:51:19 -0400

    solution upto triangle2 project

--- triangle.rb
+++ triangle.rb
@@ -12,9 +12,43 @@
 #   about_triangle_project.rb
 # and
 #   about_triangle_project_2.rb
-#
+def invalid_length?(a, b, c)
+  a <=0 or b <=0 or c<= 0 
+end
+
+def is_triangle?(other_side_len, equal_side_len)
+  if other_side_len > equal_side_len
+    raise TriangleError, 'Not a triangle'
+  end
+  true
+end
+
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if invalid_length? a, b, c
+    raise TriangleError, 'Invalid side length'
+  end
+
+  counter = 0
+  if a == b and is_triangle?(c, a)
+    counter += 1
+  end
+
+  if b == c and is_triangle?(a, b)
+    counter += 1
+  end
+
+  if c == a and is_triangle?(b, c)
+    counter += 1
+  end
+
+  if counter >= 2
+    :equilateral
+  elsif counter == 1
+    :isosceles
+  else
+    :scalene
+  end
+
 end
 
 # Error class used in part 2.  No need to change this code.
